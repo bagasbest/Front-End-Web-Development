43775703a4247ed8320ba05cba6c2dbe69502a1a
bagasbest <bagasbest80@gmail.com>
2020-05-01 16:57:56 +0700

    update kalkulator web

---
+++ dasproweb/k_flexbox/assets/latihan.js
@@ -0,0 +1,19 @@
+// let arr = ["cokelat", "stroberi", "anggur", "jeruk", "nanas"];
+// console.log("panjang array adalah : " +
+//     arr.length);
+
+// let objek = {
+//     name: "Bagas Pangestu",
+//     age: 20,
+//     isMuggle: false,
+//     isStuff: ["Magic Wind", "Flying Car", "Owl"]
+// };
+
+// console.log("Halo nama saya " + objek.name);
+// console.log("umur saya " + objek.age);
+
+
+let x = 10,
+    y = 10;
+
+console.log("x + " + "y " + " = ", x + y);
